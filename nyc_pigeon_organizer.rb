60364353d1a739165b5817402dbfbcb2dbff0095
“Diljeet <jeetds1994@gmail.com>
2017-05-25 00:46:51 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  nameList = []
+  answer = {}
+  data.each do |key, types|
+    types.each do |colors, nameArr|
+      nameArr.each do |name|
+        nameList.push(name)
+      end
+    end
+  end
+
+  nameList.each do |name|
+    answer[name] = Hash.new { |h, k| h[k] = []}
+    data.each do |key, types|
+      types.each do |colors, nameArr|
+
+        puts "#{name}, #{key}, #{colors}"
+        if data[key][colors].include?(name)
+
+          answer[name][key].push(colors.to_s)
+
+        end
+      end
+    end
+  end
+
+  answer
 end
